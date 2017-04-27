ef042b87d35f47d1e79008141b176254ab69bbd9
Christoph Müller <c.mueller@data-team.de>
2017-04-27 15:28:17 +0200

    init

---
+++ component.js
@@ -0,0 +1,48 @@
+const mkdirp = require('mkdirp')
+const template = require('lodash/template')
+const fs = require('fs')
+
+const index = `import <% name %>Component from './<% nameLower %>'
+
+import { updateSimpleField } from '../../../actions'
+import { connect } from 'react-redux'
+
+const mapStateToProps = (state, ownProps) => ({
+  simpleFields: state.simpleFields,
+})
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  updateSimpleField(field, value) {
+    dispatch(updateSimpleField(field)(value))
+  },
+})
+
+const <% name %> = connect(mapStateToProps, mapDispatchToProps)(<% name %>Component)
+
+export default <% name %>
+`
+
+const component = `import React from 'react'
+
+const <% name %> = ({
+  simpleFields,
+  updateSimpleField,
+}) => {
+  return (
+    <div>
+      Yo!
+    </div>
+  )
+}
+
+export default <% name %>
+`
+
+const name = process.argv[2]
+const nameLower = name.toLowerCase()
+const path = process.cwd()
+
+mkdirp(`${path}/${name}`, e => {
+  fs.writeFile(`${path}/${name}/index.js`, template(index)({ name, nameLower }), console.log)
+  fs.writeFile(`${path}/${name}/${nameLower}.js`, template(component)({ name }), console.log)
+})
